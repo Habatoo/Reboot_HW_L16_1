3c877b765532ac5f71b1bd13c24bac7542bdc5e3
habatoo <habatoo@gmail.com>
2021-09-26 13:54:04 +0700

    - ServletImpl

--- src/main/java/Laptenkov/Servlet.java
+++ src/main/java/Laptenkov/Servlet.java
@@ -16,4 +16,67 @@ import java.io.IOException;
 @WebServlet("/methods")
 public class Servlet extends HttpServlet {
 
+    /**
+     * Метод GET - тестовый метод - возвращает сообщение текстом "Method GET"
+     * при запросе GET по адресу http://localhost:8080/HW_16/methods
+     * @param req запрос по адресу http://localhost:8080/HW_16/methods
+     * @param resp ответ при запросе по адресу http://localhost:8080/HW_16/methods
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletOutputStream outputStream = resp.getOutputStream();
+        outputStream.println("Method " + req.getMethod());
+        outputStream.println("Request URI " + req.getRequestURI());
+        outputStream.close();
+    }
+
+    /**
+     * Метод POST - тестовый метод - возвращает сообщение текстом "Method POST"
+     * при запросе POST по адресу http://localhost:8080/HW_16/methods
+     * @param req запрос по адресу http://localhost:8080/HW_16/methods
+     * @param resp ответ при запросе по адресу http://localhost:8080/HW_16/methods
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletOutputStream outputStream = resp.getOutputStream();
+        outputStream.println("Method " + req.getMethod());
+        outputStream.println("Request URI " + req.getRequestURI());
+        outputStream.close();
+    }
+
+    /**
+     * Метод PUT - тестовый метод - возвращает сообщение текстом "Method PUT"
+     * при запросе PUT по адресу http://localhost:8080/HW_16/methods
+     * @param req запрос по адресу http://localhost:8080/HW_16/methods
+     * @param resp ответ при запросе по адресу http://localhost:8080/HW_16/methods
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletOutputStream outputStream = resp.getOutputStream();
+        outputStream.println("Method " + req.getMethod());
+        outputStream.println("Request URI " + req.getRequestURI());
+        outputStream.close();
+    }
+
+    /**
+     * Метод DELETE - тестовый метод - возвращает сообщение текстом "Method DELETE"
+     * при запросе DELETE по адресу http://localhost:8080/HW_16/methods
+     * @param req запрос по адресу http://localhost:8080/HW_16/methods
+     * @param resp ответ при запросе по адресу http://localhost:8080/HW_16/methods
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletOutputStream outputStream = resp.getOutputStream();
+        outputStream.println("Method " + req.getMethod());
+        outputStream.println("Request URI " + req.getRequestURI());
+        outputStream.close();
+    }
 }
