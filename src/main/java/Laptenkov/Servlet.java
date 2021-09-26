b3b3fb87494c4e248c8d9932eb7ab238f2951e8a
habatoo <habatoo@gmail.com>
2021-09-26 13:09:28 +0700

    initial commit

---
+++ src/main/java/Laptenkov/Servlet.java
@@ -0,0 +1,19 @@
+package Laptenkov;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Сервлет - обрабатывает методы
+ * (GET, POST, PUT, DELETE).
+ * @author habatoo
+ */
+@WebServlet("/methods")
+public class Servlet extends HttpServlet {
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>HW Lesson 16, part 1</h2>
+
+</body>
+</html>
